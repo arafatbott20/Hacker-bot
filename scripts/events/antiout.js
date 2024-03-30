37e01295c15e6adc463b7fcf8dedf00d1b202c69
arafatbott20 <161183033+arafatbott20@users.noreply.github.com>
2024-03-30 15:29:41 +0600

    Update antiout.js

--- scripts/events/antiout.js
+++ scripts/events/antiout.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "antiout",
  eventType: ["log:unsubscribe"],
  version: "0.0.1",
- credits: "Nayan",
+ credits: "DungUwU",
  description: "Listen events"
 };
 
@@ -11,12 +11,14 @@ module.exports.run = async({ event, api, Threads, Users }) => {
  if (data.antiout == false) return;
  if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
  const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
- const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "being kicked by the administrator na pasikat";
+ const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "being kicked by the administrator";
  if (type == "self-separation") {
   api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
    if (error) {
-    api.sendMessage(`Unable to re-add members ${name} to the group\n\n${name} blocked me or There is no Message option in the profile `, event.threadID)
-   } else api.sendMessage(`${name} 𝘀𝘁𝘂𝗽𝗶𝗱 𝘆𝗼𝘂 𝗵𝗮𝘃𝗲 𝗻𝗼 𝗲𝘀𝗰𝗮𝗽𝗲 𝗳𝗿𝗼𝗺 𝗵𝗲𝗿𝗲`, event.threadID);
+    api.sendMessage(`সোনা ${name} কেন পিরিতি বাড়াইলারে বন্ধু 
+
+ছেড়ে যাইবা যদি☹️🤖`, event.threadID)
+   } else api.sendMessage(`${name}  তোমাকে ছাড়া আমার ভালো লাগে না তাই আবার এড দিলাম জানু🥲🫰😘`, event.threadID);
   })
  }
 }
